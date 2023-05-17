ccf714e7d6d58bfac4dbc86143316507b5f83f98
ddimdim <123562368+ddimdim@users.noreply.github.com>
2023-05-17 18:39:06 +0500

    Add files via upload

---
+++ Collections/Queue/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Queue_students
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Clear();
+                Queue good_students = new Queue();
+                Queue bad_students = new Queue();
+                StreamReader sr = File.OpenText("students.txt");
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] info = line.Split(' ');
+                    string surname = info[0];
+                    string name = info[1];
+                    string otchestvo = info[2];
+                    string group = info[3];
+                    int grade1 = Convert.ToInt32(info[4]);
+                    int grade2 = Convert.ToInt32(info[5]);
+                    int grade3 = Convert.ToInt32(info[6]);
+                    if (grade1 <= 2 || grade2 <= 2 || grade3 <= 2)
+                    {
+                        bad_students.Enqueue($"Студент: {surname} {name} {otchestvo} {group}   Результаты сессии: РМП - {grade1}, ПиТПМ - {grade2}, ОПБД - {grade3}");
+                    }
+                    else
+                    {
+                        good_students.Enqueue($"Студент: {surname} {name} {otchestvo} {group}   Результаты сессии: РМП - {grade1}, ПиТПМ - {grade2}, ОПБД - {grade3}");
+                    }
+
+                }
+                listBox1.Items.Add("----------------- Студенты, успешно сдавшие сессию -------------------");
+                foreach (string str in good_students)
+                {
+                    listBox1.Items.Add(str);
+                }
+                listBox1.Items.Add("----------------- Студенты, сдавшие сессию неудачно -------------------");
+                foreach (string str in bad_students)
+                {
+                    listBox1.Items.Add(str);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Файл не был обнаружен", "Ошибка");
+            }
+            
+        }
+    }
+}
---
+++ Collections/Stack/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Stack<int> numbers = new Stack<int>();
+                Stack<char> mp = new Stack<char>();
+                StreamReader sr = new StreamReader("math.txt");
+                string line = sr.ReadLine();
+                foreach (char ch in line)
+                {
+                    if (ch == 'm' || ch == 'p')
+                    {
+                        mp.Push(ch);
+                    }
+                    else if (char.IsDigit(ch))
+                    {
+                        numbers.Push(int.Parse(ch.ToString()));
+                    }
+                    else if (ch == ')')
+                    {
+                        int b = numbers.Pop();
+                        int a = numbers.Pop();
+                        if (mp.Pop() == 'p')
+                        {
+                            numbers.Push((a + b) % 10);
+                        }
+                        else 
+                        {
+                            numbers.Push((a - b) % 10);
+                        }
+                    }
+                }
+                Console.WriteLine($"{line} = {numbers.Pop()}");
+                sr.Close();
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Файл с примером был не найден или пример был введен некорректно");
+                Console.ReadKey();
+            }
+        }
+    }
+}
